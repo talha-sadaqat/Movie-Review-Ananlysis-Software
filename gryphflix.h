38ca0553f1f825eadb805bb75e0d053511d1cd54
Muhamamd Talha Sadaqat <97758518+talha-sadaqat@users.noreply.github.com>
2023-02-16 20:15:33 -0500

    Update gryphflix.h

--- gryphflix.h
+++ gryphflix.h
@@ -1,17 +1,5 @@
 /*
 Student Name: MUHAMMAD TALHA SADAQAT
-Student ID: 1145327
-Due Date: Friday, 2021
-Course: CIS*2500
-I have exclusive control over this submission via my password.
-By including this header comment, I certify that:
-1) I have read and understood the policy on academic integrity.
-2) I have completed Moodle's module on academic integrity.
-3) I have achieved at least 80% on the academic integrity quiz
-I assert that this work is my own. I have appropriate acknowledged
-any and all material that I have used, be it directly quoted or
-paraphrased. Furthermore, I certify that this assignment was written
-by me in its entirety.
 */
 
 #define NUMBER_REVIEWERS 10
